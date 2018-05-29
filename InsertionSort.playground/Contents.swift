e645cd08dd95d1e29251de7dcff765bb0d3a5f7b
amrish036 <amrish.036@gmail.com>
2018-05-29 21:19:12 +1000

    Create Generics for Insertion sort

---
+++ InsertionSort.playground/Contents.swift
@@ -0,0 +1,18 @@
+func insertionSort<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+var a = array
+
+for x in 1..<a.count{
+  var y = x
+  let temp = a[y]
+       while y > 0 && isOrderedBefore(temp, a[y - 1]) {
+    a[y] = a[y-1]
+    y -= 1
+  }
+  a[y] = temp
+}
+return a
+}
+
+let numbers = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSort(numbers, <)
+insertionSort(numbers, >)
