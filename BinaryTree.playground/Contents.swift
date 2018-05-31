9fafe4738c89c40e19fa3b547a6407a7b541b4ce
amrish036 <amrish.036@gmail.com>
2018-05-31 21:31:45 +1000

    Create Binary tree generic and insert items to tree

---
+++ BinaryTree.playground/Contents.swift
@@ -0,0 +1,93 @@
+enum BinaryTree<T: Comparable>{
+  case empty
+  indirect case node(BinaryTree, T, BinaryTree)
+  
+  var count: Int {
+    switch self {
+    case let .node(left, _, right):
+      return left.count + 1 + right.count
+    case .empty:
+      return 0
+    }
+  }
+  
+  // 1. 
+  mutating func naiveInsert(newValue: T) {
+    // 2.
+    guard case .node(var left, let value, var right) = self else {
+      // 3. 
+      self = .node(.empty, newValue, .empty)
+      return 
+    }
+    // 4. TODO: Implement rest of algorithm!
+    if newValue < value {
+      left.naiveInsert(newValue: newValue)
+    } else {
+      right.naiveInsert(newValue: newValue)
+    
+  }
+  
+}
+  
+  private func newTreeWithInsertedValue(newValue: T) -> BinaryTree {
+    switch self {
+    // 1
+    case .empty:
+      return .node(.empty, newValue, .empty)
+    // 2
+    case let .node(left, value, right):
+      if newValue < value {
+        return .node(left.newTreeWithInsertedValue(newValue: newValue), value, right)
+      } else {
+        return .node(left, value, right.newTreeWithInsertedValue(newValue: newValue))
+      }
+    }
+  }
+  
+  mutating func insert(newValue: T){
+    self = newTreeWithInsertedValue(newValue: newValue)
+  }
+  
+}
+
+// leaf nodes
+let node5 = BinaryTree.node(.empty, "5", .empty)
+let nodeA = BinaryTree.node(.empty, "a", .empty)
+let node10 = BinaryTree.node(.empty, "10", .empty)
+let node4 = BinaryTree.node(.empty, "4", .empty)
+let node3 = BinaryTree.node(.empty, "3", .empty)
+let nodeB = BinaryTree.node(.empty, "b", .empty)
+
+// intermediate nodes on the left
+let Aminus10 = BinaryTree.node(nodeA, "-", node10)
+let timesLeft = BinaryTree.node(node5, "*", Aminus10)
+
+// intermediate nodes on the right
+let minus4 = BinaryTree.node(.empty, "-", node4)
+let divide3andB = BinaryTree.node(node3, "/", nodeB)
+let timesRight = BinaryTree.node(minus4, "*", divide3andB)
+
+// root node
+let tree = BinaryTree.node(timesLeft, "+", timesRight)
+
+tree.count
+
+extension BinaryTree: CustomStringConvertible {
+  var description: String {
+    switch self {
+    case let .node(left, value, right):
+      return "value: \(value), left = [" + left.description + "], right = [" + right.description + "]"
+    case .empty:
+      return ""
+    }
+  }
+}
+
+//print(tree)
+
+var binaryTree: BinaryTree<Int> = .empty
+binaryTree.insert(newValue: 5)
+binaryTree.insert(newValue: 7)
+binaryTree.insert(newValue: 9) 
+
+print(binaryTree)
